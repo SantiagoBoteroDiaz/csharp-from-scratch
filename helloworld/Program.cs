8ff455f31d50338984fcd8ca253654a2849559bd
SantiagoBotero <Santiagoboterito@gmail.com>
2025-02-07 17:18:31 -0500

    change hello world by class

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace HelloWorld{
+    class Program {
+       
+        static void Main (){
+            const string Name= "Jhon";
+            Console.WriteLine($"Hello world! {Name}");
+            Console.WriteLine();
+
+        }
+    }
+}
